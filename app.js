952af19852326e9d8c71b965c103a0e26db620b2
rakeshgali <rakeshnagasaigali@gmail.com>
2022-07-01 06:58:44 +0000

    movies

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ const dbPath = path.join(__dirname, "moviesData.db");
 
 const app = express();
 
-module.exports = app;
-
-app.use(express.json());
-
 let dataBase = null;
 
 const initializeDbAndServer = async () => {
@@ -31,6 +27,7 @@ const initializeDbAndServer = async () => {
 };
 
 initializeDbAndServer();
+app.use(express.json());
 
 const convertMovieDbObjectToResponseObject = (dbObject) => {
   return {
@@ -47,7 +44,7 @@ app.get("/movies/", async (request, response) => {
     from movie;`;
 
   const moviesArray = await dataBase.all(dbQuery);
-  response.send(moviesArray);
+  response.send(movieArray);
 });
 
 app.post("/movies/", async (request, response) => {
@@ -62,7 +59,7 @@ app.post("/movies/", async (request, response) => {
 });
 
 app.get("/movies/:movieId/", async (request, response) => {
-  const movieId = request.params;
+  const { movieId } = request.params;
   const movieQuery = `
     SELECT *
     FROM MOVIE
@@ -70,5 +67,56 @@ app.get("/movies/:movieId/", async (request, response) => {
 
   const movie = await dataBase.get(movieQuery);
 
-  response.send(convertMovieDbObjectToResponseObject(movie));
+  response.send(movie);
 });
+
+app.put("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const { directorId, movieName, leadActor } = request.body;
+
+  const updateQuery = `update movie
+    set 
+    director_id=${directorId},
+    movie_name='${movieName}',
+    lead_actor='${leadActor}'
+    where
+    movie_id=${movieId};`;
+
+  await dataBase.run(updateQuery);
+  respond.send("updated successfully");
+});
+
+app.delete("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const deleteQuery = `delete *
+        from movie
+        where movie_id=${movieId}`;
+
+  await dataBase.run(deleteQuery);
+  response.send("movie deleted");
+});
+
+app.get("/directors/", async (request, response) => {
+  const dbQuery = `
+    select *
+    from director;`;
+
+  const directorsArray = await dataBase.all(dbQuery);
+  response.send(directorsArray);
+});
+
+app.get("/directors/:directorId/movies/", async (request, response) => {
+  const { directorId } = request.params;
+  const dirQuery = `
+    select 
+        movie_name
+    from
+    movie 
+    where 
+    director_id=${directorId}`;
+
+  const movieName = await dataBase.get(dirQuery);
+  response.send({ movie_name: movieName });
+});
+
+module.exports = app;
